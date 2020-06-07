f30a03b4c03a6cc7cbff738aa9ae0b9d257b4ff0
emkoltsova <kolcova.elizaveta@gmail.com>
2020-06-07 23:37:38 +0300

    change test_items, add assert

--- test_items.py
+++ test_items.py
@@ -4,8 +4,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 class TestMainPage1():
     # вызываем фикстуру в тесте, передав ее как параметр
-    def test_button(self, browser):
+    def test_find_button(self, browser):
         browser.get(link)
-        #time.sleep(5)
+        time.sleep(10)
         #в зависимости от параметра language при запуске меняется язык кнопок, запуск - pytest --language=es test_items.py
-        browser.find_element_by_css_selector(".btn-primary")
+        button = browser.find_element_by_css_selector(".btn-primary")
+        assert button.text!=None, "Button is not found"
